9fce2e7900ac276fa87f7cb16d22cd0d925394a8
moro23 <billhab66@gmail.com>
2025-11-12 16:41:27 +0000

    Add custom permission to allow only authors to edit their posts

---
+++ backend/posts/permissions.py
@@ -0,0 +1,14 @@
+##backend/post/permissions.py 
+from rest_framework import permissions 
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        ### 
+        if request.method in permissions.SAFE_METHODS:
+            return True 
+        
+        # Write permissions are only allowed to the author of the post 
+        return obj.author == request.user 
