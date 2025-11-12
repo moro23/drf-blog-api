9bfc6d97e788e098c0a0f2fe879dfd094b41e757
moro23 <billhab66@gmail.com>
2025-11-12 16:40:35 +0000

    Make author field read-only in PostSerializer

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -4,6 +4,10 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
+
+        ## lets make the author field read-only
+        author = serializers.StringRelatedField(read_only=True)
+
         fields = ('id', 'author', 'title', 'body', 'created_at')
         model = Post 
 
