9f4e26155f02bda25685d88f8914a41e84983c4f
moro23 <billhab66@gmail.com>
2025-11-12 09:53:26 +0000

    Add JWT authentication to REST framework settings

--- backend/blog_project/settings.py
+++ backend/blog_project/settings.py
@@ -51,7 +51,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
         'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ], 
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.JWTAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
