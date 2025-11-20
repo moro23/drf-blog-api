0a97521218a1c380ef68ae7cacbe8aecc08a44b8
moro23 <billhab66@gmail.com>
2025-11-20 10:52:04 +0000

    Add UserSerializer and implement author-only permissions for post updates

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model 
 from rest_framework import serializers 
 from .models import Post 
 
@@ -11,3 +12,7 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ('id', 'author', 'title', 'body', 'created_at')
         model = Post 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model() 
+        fields = ('id', 'username')
