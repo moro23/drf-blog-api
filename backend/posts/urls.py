669af64feee4d4d2494337f7a2b080a8f2a387ec
moro23 <billhab66@gmail.com>
2025-11-20 10:52:18 +0000

    Refactor URL configuration to use SimpleRouter for Post and User viewsets

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path 
 
-from .views import PostList, PostDetail
+from rest_framework.routers import SimpleRouter 
 
-urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+from .views import PostViewSet, UserViewSet
+
+# from .views import PostList, PostDetail, UserList, UserDetail
+
+# urlpatterns = [
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path('<int:pk>/', PostDetail.as_view()),
+#     path('', PostList.as_view()),
+    
+# ]
+
+router = SimpleRouter() 
+
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls 
